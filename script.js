87d5a45aa9b878db3c04ab5277569e851ddde2c7
mattiglody <mattiglody@outlook.com>
2025-08-26 08:55:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -474,12 +474,12 @@ volumeControl.oninput = () => {
 
 // --- Image Viewer Logic ---
 const photoAlbum = [
-  { src: 'pics/photography/delgap.jpg', caption: 'South West NJ Coast, Del Water Gap' },
-  { src: 'pics/photography/morntide.jpg', caption: 'Newport Jersey City Walkway' },
-  { src: 'pics/photography/moonset.jpg', caption: 'Hoboken Fire Escape' },
-  { src: 'pics/photography/nycsuns.jpg', caption: 'Midtown Sunset' },
-  { src: 'pics/photography/sherbsky.jpg', caption: 'Hoboken Fire Escape' },
-  { src: 'pics/photography/wintrrd.jpg', caption: 'Adirondack Northway in Winter' }
+  { src: 'https://mattiglody.github.io/pics/photography/delgap.jpg', caption: 'South West NJ Coast, Del Water Gap' },
+  { src: 'https://mattiglody.github.io/pics/photography/morntide.jpg', caption: 'Newport Jersey City Walkway' },
+  { src: 'https://mattiglody.github.io/pics/photography/moonset.jpg', caption: 'Hoboken Fire Escape' },
+  { src: 'https://mattiglody.github.io/pics/photography/nycsuns.jpg', caption: 'Midtown Sunset' },
+  { src: 'https://mattiglody.github.io/pics/photography/sherbsky.jpg', caption: 'Hoboken Fire Escape' },
+  { src: 'https://mattiglody.github.io/pics/photography/wintrrd.jpg', caption: 'Adirondack Northway in Winter' }
 ];
 let currentPhotoIndex = 0;
 
