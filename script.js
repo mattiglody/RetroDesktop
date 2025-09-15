6c87c25816f0540e73fafdbeb9d827f47a3669af
mattiglody <mattiglody@outlook.com>
2025-09-15 10:01:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -634,9 +634,9 @@ const audioElement = document.getElementById('media-player-audio');
 
 // --- Playlist Configuration ---
 const playlist = [
-    { name: "zwan_love_lies_in_ruin_acoustic_2003.mp3", path: 'media/zwan_love_lies_in_ruin_acoustic_2003.mp3' },
-    { name: "Praise_You_Fatboy_Slim.mp3", path: 'media/Praise_You_Fatboy_Slim.mp3' },
-    { name: "Jethro_Tull_Teacher.mp3", path: 'media/Jethro_Tull_Teacher.mp3' },
+    { name: "zwan_love_lies_in_ruin_acoustic_2003.mp3", path: './media/zwan_love_lies_in_ruin_acoustic_2003.mp3' },
+    { name: "praise_you_fatboy_slim.mp3", path: './media/praise_you_fatboy_slim.mp3' },
+    { name: "jethro_tull_teacher.mp3", path: './media/jethro_tull_teacher.mp3' },
     // Add more songs here
 ];
 
@@ -868,12 +868,12 @@ volumeControl.oninput = () => {
 
 // --- Image Viewer Logic ---
 const photoAlbum = [
-  { src: 'pics/photography/delgap.jpg', caption: 'South West NJ Coast, Del Water Gap' },
-  { src: 'pics/photography/morntide.jpg', caption: 'Newport Jersey City Walkway' },
-  { src: 'pics/photography/moonset.jpg', caption: 'Hoboken Fire Escape' },
-  { src: 'pics/photography/nycsuns.jpg', caption: 'Midtown Sunset' },
-  { src: 'pics/photography/sherbsky.jpg', caption: 'Hoboken Fire Escape' },
-  { src: 'pics/photography/wintrrd.jpg', caption: 'Adirondack Northway in Winter' }
+  { src: './pics/photography/delgap.jpg', caption: 'South West NJ Coast, Del Water Gap' },
+  { src: './pics/photography/morntide.jpg', caption: 'Newport Jersey City Walkway' },
+  { src: './pics/photography/moonset.jpg', caption: 'Hoboken Fire Escape' },
+  { src: './pics/photography/nycsuns.jpg', caption: 'Midtown Sunset' },
+  { src: './pics/photography/sherbsky.jpg', caption: 'Hoboken Fire Escape' },
+  { src: './pics/photography/wintrrd.jpg', caption: 'Adirondack Northway in Winter' }
 ];
 let currentPhotoIndex = 0;
 
